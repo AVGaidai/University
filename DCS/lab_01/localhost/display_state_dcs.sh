45f85e16bd6626b45b1ee37f10043dad930213d4
AVGaidai <diligent20494@gmail.com>
2016-10-25 21:03:14 +0700

    Edit report for lab_01

--- DCS/lab_01/localhost/display_state_dcs.sh
+++ DCS/lab_01/localhost/display_state_dcs.sh
@@ -2,7 +2,7 @@
 
 clear
 
-ssh gaidai@jet.cpct.sibsutis.ru "./DCS/lab_01/print_state_info.sh"
+ssh gaidai@jet.cpct.sibsutis.ru "./DCS/lab_01/print_state_dcs.sh"
 scp gaidai@jet.cpct.sibsutis.ru:~/DCS/lab_01/.pbsnodes_info ./
 
 cat .pbsnodes_info
