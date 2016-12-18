8952548633e13f40ac87b30ea13b914e697254f7
AVGaidai <diligent20494@gmail.com>
2016-12-18 13:58:17 +0700

    lab_03

---
+++ DCS/lab_03/store/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+
+int time = 60;
+int N = 5;
+int K = 2;
+
+int main (int argc, char *argv[])
+{
+    int opt;
+
+    while ((opt = getopt (argc, argv, "n:k:t:")) != -1) {
+        switch (opt) {
+        case 'c':
+
+    return 0;
+}
