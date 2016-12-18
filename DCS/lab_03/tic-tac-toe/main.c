568037d1567de828a8c7b081d3bf044ef027834a
AVGaidai <anatoly@laptop.Dlink>
2016-12-19 00:41:42 +0700

    ...

---
+++ DCS/lab_03/tic-tac-toe/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#include <pthread.h>
+
+
+int field[9] = { 0, 0, 0,
+                 0, 0, 0,
+                 0, 0, 0  };
+
+
+
+void *game (void *arg)
+{
+
+}
+
+
+
+int main (int argc, char *argv[])
+{
+
+
+    return 0;
+}
